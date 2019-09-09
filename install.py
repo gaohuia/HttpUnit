57654c4dc526372e98f72342347b6101a48a849f
Tom Zhu <tom.zhu@planetart.cn>
2019-09-09 23:15:54 +0800

    install code

---
+++ install.py
@@ -0,0 +1,14 @@
+import urllib.request,os,hashlib,tempfile,zipfile,shutil;
+version = "1.1.0";
+name = "SimpleHttpRequester";
+url = "https://github.com/gaohuia/SimpleHttpRequester/archive/v%s.zip" % (version);
+pp = sublime.packages_path();
+urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) );
+by = urllib.request.urlopen(url).read();
+io = tempfile.TemporaryFile();
+io.write(by);
+temp_dir = tempfile.gettempdir();
+z = zipfile.ZipFile(io);
+z.extractall(temp_dir);
+shutil.copytree(temp_dir + "/" + name + "-" + version, pp + "/" + name);
+io.close();
